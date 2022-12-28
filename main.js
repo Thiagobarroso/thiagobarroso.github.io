8d98f0335b15a33be49d2cb7bea6f43a003eb6f0
Thiagobarroso <thiagoo.silva@live.com>
2022-12-28 00:27:59 -0300

    add scroll suave

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
 
-function scrollToSection(event) {
-    event.preventDefault(); 
-    const href = event current.getAtribute('href');
-    const section = document.querySelector(href);
+  function scrollToSection(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
 
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
   }
 
-linksInternos forEach((link) => {
-  link.addEventListener('click', scrollToSection);
-})
+  linksInternos.forEach(link => {
+    link.addEventListener('click', scrollToSection)
+  })
+}
+
+initScrollSuave()
