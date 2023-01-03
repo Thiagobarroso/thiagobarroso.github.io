9e92edefcb68e1f9f32947290b4b8fa3b83eb152
thiagobarroso <thiagoo.silva@live.com>
2023-01-03 13:04:27 -0300

    btn baixar curriculo

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
   function scrollToSection(event) {
-    event.preventDefault()
-    const href = event.currentTarget.getAttribute('href')
-    const section = document.querySelector(href)
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
 
     section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
+      behavior: "smooth",
+      block: "start",
+    });
   }
 
-  linksInternos.forEach(link => {
-    link.addEventListener('click', scrollToSection)
-  })
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
 }
 
-initScrollSuave()
+initScrollSuave();
