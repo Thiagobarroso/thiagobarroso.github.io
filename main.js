f50e5818a551b353c38ae1ad2e43aec7c181ff2b
Thiagobarroso <thiagoo.silva@live.com>
2023-01-22 23:38:18 -0300

    adicionado botao dark/light

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
 function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
 
   function scrollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute("href");
-    const section = document.querySelector(href);
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
 
     section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
+      behavior: 'smooth',
+      block: 'start'
+    })
   }
 
-  linksInternos.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
+  linksInternos.forEach(link => {
+    link.addEventListener('click', scrollToSection)
+  })
 }
 
-initScrollSuave();
+initScrollSuave()
+
+const dark = document.querySelector('button')
+let status = 'ativo'
+
+dark.addEventListener('click', function () {
+  document.body.classList.toggle('dark')
+})
+
+dark.addEventListener('click', function () {
+  if (this.status === 'ativo') {
+    dark.innerHTML = 'dark_mode'
+    this.status = 'desativado'
+  } else {
+    dark.innerHTML = 'light_mode'
+    this.status = 'ativo'
+  }
+})
