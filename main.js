01151deaebed878edfba6e184ef241f09b325aca
Thiago Barroso da Silva <thiagoo.silva@live.com>
2023-08-07 02:29:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,35 @@
 function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
   function scrollToSection(event) {
-    event.preventDefault()
-    const href = event.currentTarget.getAttribute('href')
-    const section = document.querySelector(href)
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
 
     section.scrollIntoView({
       behavior: 'smooth',
       block: 'start'
-    })
+    });
   }
 
   linksInternos.forEach(link => {
-    link.addEventListener('click', scrollToSection)
-  })
+    link.addEventListener('click', scrollToSection);
+  });
 }
 
-initScrollSuave()
+initScrollSuave();
 
-const dark = document.querySelector('button')
-let status = 'ativo'
-
-dark.addEventListener('click', function () {
-  document.body.classList.toggle('dark')
-})
+const dark = document.querySelector('button');
+let status = 'ativo';
 
 dark.addEventListener('click', function () {
-  if (this.status === 'ativo') {
-    dark.innerHTML = 'dark_mode'
-    this.status = 'desativado'
+  document.body.classList.toggle('dark');
+
+  if (status === 'ativo') {
+    dark.innerHTML = 'dark_mode';
+    status = 'desativado';
   } else {
-    dark.innerHTML = 'light_mode'
-    this.status = 'ativo'
+    dark.innerHTML = 'light_mode';
+    status = 'ativo';
   }
-})
+});
