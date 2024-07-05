46e9e9d2680928c8dabf0ce85e0b2cd78c2173ea
Thiago Barroso <thiagoo.silva@live.com>
2024-07-05 12:07:51 -0300

    atualização redes sociais e curriculo

--- main.js
+++ main.js
@@ -8,28 +8,13 @@ function initScrollSuave() {
 
     section.scrollIntoView({
       behavior: 'smooth',
-      block: 'start'
+      block: 'start',
     });
   }
 
-  linksInternos.forEach(link => {
+  linksInternos.forEach((link) => {
     link.addEventListener('click', scrollToSection);
   });
 }
 
 initScrollSuave();
-
-const dark = document.querySelector('button');
-let status = 'ativo';
-
-dark.addEventListener('click', function () {
-  document.body.classList.toggle('dark');
-
-  if (status === 'ativo') {
-    dark.innerHTML = 'dark_mode';
-    status = 'desativado';
-  } else {
-    dark.innerHTML = 'light_mode';
-    status = 'ativo';
-  }
-});
